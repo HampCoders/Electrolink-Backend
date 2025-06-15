04ada75c8ab718563ea78f450db56497b4046b01
MatFragg <u202318323@upc.edu.pe>
2025-06-14 19:05:25 -0500

    feat: Add value objects for Componenets Type, Owner Id and Technician Id

---
+++ Hampcoders.Electrolink.API/Assets/Domain/Model/ValueObjects/EComponentType.cs
@@ -0,0 +1,21 @@
+namespace Hampcoders.Electrolink.API.Assets.Domain.Model.ValueObjects;
+
+public enum EComponentType
+{
+    Cable,              
+    Breaker,              
+    Switch,               
+    Socket,               
+    Lightbulb,           
+    LightFixture,         
+    OutletBox,             
+    Fuse,                  
+    ElectricalPanel,       
+    Capacitor,             
+    Resistor,             
+    VoltageRegulator,     
+    SmartDevice,           
+    Transformer,           
+    GroundingRod,          
+    SurgeProtector         
+}
